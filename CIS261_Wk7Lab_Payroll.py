cb78ac0e7ef7e5a9a3855b571713808a84488e72
Tyler Johnson <Tyler.Johnson1@my.strayer.edu>
2022-08-21 11:27:41 -0400

    FINAL COMMIT FOR WEEK 7

--- CIS261_Wk7Lab_Payroll.py
+++ CIS261_Wk7Lab_Payroll.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-def convert_from_p_del_to_list(data,date):
+def convert_from_p_del_to_list(data,entered_date):
     line_break_split = data.split('\n')
     line_break_split.pop()
+    new_list = []
     for l in line_break_split:
         new_split = l.split('|')
-        new_list = []
         from_date = new_split[0]
         to_date = new_split[1]
         name = new_split[2]
@@ -15,27 +15,26 @@ def convert_from_p_del_to_list(data,date):
             "from_date"     :   from_date,
             "to_date"       :   to_date,
             "name"          :   name,
-            "hoursworked"   :   totalhours,
-            "hourlyrate"    :   hourlyrate,
-            "incometaxrate" :   incometaxrate
+            "hoursworked"   :   float(totalhours),
+            "hourlyrate"    :   float(hourlyrate),
+            "incometaxrate" :   float(incometaxrate)
             }
-        if date.lower()=="all" or datetime.strptime(date, "%m/%d/%Y") <= datetime.strptime(new_dict["from_date"], "%m/%d/%Y"):
-            print(f'{name}\'s time matches what was entered')
+        if entered_date == "all" or entered_date <= datetime.strptime(new_dict["from_date"], "%m/%d/%Y"):
             new_list.append(new_dict)
-        else:
-            pass
+    return(new_list)
+            
 def get_search_date():
-    report_search_date = input('Search Date (mm/dd/yyyy) or All: ')
-
     file = open("payroll.txt", 'r')
     data = file.read()
-    data_to_read = convert_from_p_del_to_list(data, report_search_date)
+    report_search_date = input('Search Date (mm/dd/yyyy) or All: ')
     if report_search_date.lower() == "all":
-        # Write a function that prints all.
-        pass
+        data_to_read = convert_from_p_del_to_list(data, report_search_date)
+        print_all_payroll(data_to_read)
     else:
         try:
             formatted_date = datetime.strptime(report_search_date, "%m/%d/%Y")
+            data_to_read = convert_from_p_del_to_list(data, formatted_date)
+            print_all_payroll(data_to_read)
         except ValueError:
             print('Invalid date format. Try again.\n')
 def write_to_file(from_date, to_date, name, hoursworked, hourlyrate, incometaxrate):
@@ -48,22 +47,20 @@ def get_hourly_rate():
     hourlyrate = float(input('Enter hourly rate: '))
     return hourlyrate
 def get_income_tax_rate():
-    incometaxrate = input('Enter income tax rate: ')
-    return float(incometaxrate)
+    incometaxrate = float(input('Enter income tax rate: '))
+    return incometaxrate
 def get_dates():
     from_date = input('Enter From Date(mm/dd/yyyy): ')
     to_date = input('Enter To Date(mm/dd/yyyy): ')
     return from_date, to_date
 def calc_gross_pay(hourlyrate, totalhours):
-    grosspay = float(hourlyrate) * float(totalhours)
+    grosspay = hourlyrate * totalhours
     return float(grosspay)
 
 def calc_income_tax(grosspay,incometaxrate):
     incometax = grosspay * incometaxrate
     return incometax
-
 def calc_net_pay(grosspay, incometax):
-
     netpay = grosspay - incometax
     return netpay
 def get_income_tax_and_net_pay(totalhours, incometaxrate, hourlyrate):
@@ -78,14 +75,14 @@ def print_all_payroll(employee_list):
     totalincometax = 0.00
     totalnetpay = 0.00
     for e in employee_list:
-        from_date = e[0]
-        to_date = e[1]
-        name = e[2]
-        totalhours = e[3]
-        hourlyrate = e[4]
-        incometaxrate = e[5]
+        from_date      =   e['from_date']
+        to_date        =   e['to_date']
+        name           =   e['name']
+        totalhours     =   e['hoursworked']
+        hourlyrate     =   e['hourlyrate']
+        incometaxrate  =   e['incometaxrate']
         incometax, netpay, grosspay = get_income_tax_and_net_pay(totalhours, incometaxrate, hourlyrate)
-        print(from_date, to_date, name, f'{totalhours:,.2f}', f'{hourlyrate:,.2f}', f'{grosspay:,.2f}', f'{incometaxrate:,.1%}', f'{incometax:,.2f}', f'{netpay:,.2f}',)
+        print(from_date, to_date, name, f'{totalhours:,.2f}', f'{hourlyrate:,.2f}', f'{grosspay:,.2f}', f'{incometaxrate:,.2f}%', f'{incometax:,.2f}', f'{netpay:,.2f}',)
         employeecount += 1
         totalhoursworked += totalhours
         totalgrosspay += grosspay
@@ -96,13 +93,11 @@ def print_all_payroll(employee_list):
         employee_totals['total_gross_pay'] = totalgrosspay
         employee_totals['total_income_tax'] = totalincometax
         employee_totals['total_net_pay'] = totalnetpay
-
-def print_payroll_totals(employee_totals):
-     print(f'Total number of employees: {employee_totals["number_of_employees"]}')
-     print(f'Total hours worked:        {employee_totals["total_hours_worked"]:,.2f}')
-     print(f'Total gross pay:           {employee_totals["total_gross_pay"]:,.2f}')
-     print(f'Total income taxes:        {employee_totals["total_income_tax"]:,.2f}')
-     print(f'Total net play:            {employee_totals["total_net_pay"]:,.2f}')
+    print(f'Total number of employees:  {employee_totals["number_of_employees"]}')
+    print(f'Total hours worked:         {employee_totals["total_hours_worked"]:,.2f}')
+    print(f'Total gross pay:            {employee_totals["total_gross_pay"]:,.2f}')
+    print(f'Total income taxes:         {employee_totals["total_income_tax"]:,.2f}')
+    print(f'Total net pay:              {employee_totals["total_net_pay"]:,.2f}')
 
 employee_totals = {}
 
@@ -111,6 +106,8 @@ while True:
     if name.upper() == 'END':
         while True:
             get_search_date()
+            break
+        break
     else:
         from_date, to_date = get_dates()
         hoursworked = get_total_hours()
